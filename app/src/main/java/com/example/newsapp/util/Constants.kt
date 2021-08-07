beafcc3ce42f1f555698e485df3a3bc3810cc322
Vitor Cezila <vitor.duarte@gsurfnet.com>
2021-08-07 16:57:46 -0300

    Just a test

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.util
 class Constants {
 
     companion object {
-        const val API_KEY = ""
+        const val API_KEY = " "
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
     }
